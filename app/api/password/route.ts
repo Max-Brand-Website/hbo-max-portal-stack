02ed81e77e9d3a2823fa428605541ecf2692480f
Adrian <adunham95@gmail.com>
2025-11-15 15:01:17 -0500

    Removed API Key

--- app/api/password/route.ts
+++ app/api/password/route.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
-import PortalPasswordEmail from '@/emails/portal-password';
-import { NextRequest, NextResponse } from 'next/server';
-import { Resend } from 'resend';
+import PortalPasswordEmail from "@/emails/portal-password";
+import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -13,58 +13,62 @@ interface WebflowFormSubmission {
   site: string;
   data: {
     [key: string]: string;
-  }
+  };
 }
 
-const base = require('airtable').base('app60Q6txHmCLVVxB');
+const base = require("airtable").base(process.env.AIRTABLE_PASSWORD);
 
 export async function POST(request: NextRequest) {
   try {
     console.log(`Received POST to /api/password`);
 
-    const { name, data } = await request.json() as WebflowFormSubmission;
+    const { name, data } = (await request.json()) as WebflowFormSubmission;
 
-    if (name === 'Request Form') {
-      console.log(`Received request for ${data['Email']}...`);
+    if (name === "Request Form") {
+      console.log(`Received request for ${data["Email"]}...`);
       console.log(JSON.stringify({ name, data }, null, 2));
 
       /* Fetch the password from Airtable */
-      const record = await base('Password').find(process.env.AIRTABLE_PASSWORD_RECORD_ID);
-      const password = record.get('Password');
+      const record = await base("Password").find(
+        process.env.AIRTABLE_PASSWORD_RECORD_ID
+      );
+      const password = record.get("Password");
       console.log(`Retrieved password: ${password} from Airtable...`);
 
       /* Send the email with the password info */
       console.log(`Attempting to send from ${process.env.FROM_ADDRESS}...`);
 
       const resendResponse = await resend.emails.send({
-        from: 'Max Brand Portal <maxbrandportal@wbd.com>',
-        to: [data['Email']],
-        subject: 'Access the Max Brand Portal',
+        from: "Max Brand Portal <maxbrandportal@wbd.com>",
+        to: [data["Email"]],
+        subject: "Access the Max Brand Portal",
         react: PortalPasswordEmail({
           url: process.env.PORTAL_URL,
-          password
-        })
+          password,
+        }),
       });
 
       console.log(`Resend response...`);
       console.log(JSON.stringify(resendResponse, null, 2));
 
       /* Log this visitor's information in Airtable */
-      const { data: emailData, error: emailError } = await base('Visitors').create([
+      const { data: emailData, error: emailError } = await base(
+        "Visitors"
+      ).create([
         {
-          "fields": {
-            "Email Address": data['Email'],
-            "Company": data['Company'],
-            "First Name": data['First Name'],
-            "Last Name": data['Last Name'],
-            "Region": data['Region'],
-            "Job Title": data['Job Title']
-          }
-        }
+          fields: {
+            "Email Address": data["Email"],
+            Company: data["Company"],
+            "First Name": data["First Name"],
+            "Last Name": data["Last Name"],
+            Region: data["Region"],
+            "Job Title": data["Job Title"],
+          },
+        },
       ]);
 
       if (emailError || !emailData) {
-        throw new Error(error.message)
+        throw new Error(error.message);
       }
 
       createResponse.forEach(function (record) {
@@ -73,7 +77,7 @@ export async function POST(request: NextRequest) {
 
       return NextResponse.json({ data, resendResponse, createResponse });
     } else {
-      return NextResponse.json({ error: 'Invalid form name' });
+      return NextResponse.json({ error: "Invalid form name" });
     }
   } catch (error) {
     console.error(error);
